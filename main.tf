32253685d70160ec3aca956e659db6068da61c31
aapaeste <aapaeste@vt.edu>
2020-01-31 21:15:08 -0500

    added terragrunt info

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+provider "aws" {
+  region = var.aws_region
+
+  # Live modules pin exact provider version; generic modules let consumers pin the version.
+  version = "= 2.40.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+
+  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
+  # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
+  #### required_version = "= 0.12.17"
+  required_version = ">= 0.12.0"
+}
+
 locals {
   is_t_instance_type = replace(var.instance_type, "/^t[23]{1}\\..*$/", "1") == "1" ? true : false
 }
